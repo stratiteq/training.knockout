c3a336d483a07ae2a6776855f472d30267ccbc09
srosengren <sebastian@rosengren.me>
2014-08-13 21:35:25 +0200

    Base

---
+++ scripts/helpers.js
@@ -0,0 +1,17 @@
+﻿var helpers = (function () {
+    return {
+        getJson: function (url, callback, error) {
+            var r = new XMLHttpRequest();
+            r.open('GET', url, true);
+            r.onreadystatechange = function () {
+                if (r.readyState === 4 && r.status === 200 && callback) {
+                    callback(r.responseText);
+                }
+                if (r.status !== 200 && error) {
+                    error(r.statusText);
+                }
+            };
+            r.send();
+        }
+    };
+})();
---
+++ scripts/schoolRepo.js
@@ -0,0 +1,16 @@
+﻿var schoolRepo = (function () {
+    return {
+        getTeachers: function () {
+            helpers.getJson('https://api.github.com/users', function (response) {
+                var teachers = JSON.parse(response);
+                return teachers.slice(0,10);
+            });
+        },
+        getStudents: function () {
+            helpers.getJson('https://api.github.com/users', function (response) {
+                var teachers = JSON.parse(response);
+                return teachers.slice(10);
+            });
+        }
+    };
+})();
