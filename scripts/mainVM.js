068966e8c02d6d23cc4d33b1068fa85d95b9ba2b
srosengren <sebastian@rosengren.me>
2014-08-13 23:27:07 +0200

    Complete code.

---
+++ scripts/mainVM.js
@@ -0,0 +1,46 @@
+﻿var mainVM = (function () {
+    var vm = {};
+
+    vm.people = ko.observableArray();
+    vm.following = ko.observableArray();
+    vm.searchFollow = ko.observableArray('');
+
+    repo.getPeople(function (people) {
+        vm.people(people);
+    });
+
+    vm.toggleFollow = function (user) {
+        if (vm.following.indexOf(user) < 0) {
+            vm.following.push(user);
+        } else {
+            vm.following.remove(user);
+        }
+    }
+
+    vm.peopleToFollow = ko.computed(function () {
+        var people = vm.people();
+        people = ko.utils.arrayFilter(people, function (user) {
+            if (vm.following.indexOf(user) > -1)
+                return false;
+            if (user.login.indexOf(vm.searchFollow()) < 0)
+                return false;
+            return true;
+        });
+        return people;
+    });
+
+    vm.timeline = ko.computed(function () {
+        var repos = [];
+        for (var i = 0; i < vm.following().length; i++)
+            repos.push.apply(repos, vm.following()[i].repos());
+
+        repos.sort(function (left,right) {
+            return new Date(left.updated_at) < new Date(right.updated_at);
+        });
+        return repos;
+    });
+
+
+    ko.applyBindings(vm);
+    return vm;
+})();
---
+++ scripts/repo.js
@@ -0,0 +1,21 @@
+﻿var repo = (function () {
+    return {
+        getPeople: function (callback) {
+            helpers.getJson('https://api.github.com/users', function (response) {
+                var people = JSON.parse(response);
+                people = people.slice(0, 10);
+
+                for (var i = 0; i < people.length; i++) {
+                    people[i].repos = ko.observableArray();
+                    (function (user) {
+                        helpers.getJson('https://api.github.com/users/' + user.login + '/repos', function (response) {
+                            user.repos(JSON.parse(response));
+                        });
+                    })(people[i]);
+                }
+
+                callback(people);
+            });
+        },
+    };
+})();
